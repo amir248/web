58084a1f2cd7d3bf3b5811040196a6f4c139a47d
chikchicly <chikchicly@gmail.com>
2026-05-12 07:00:01 +0700

    Backup 2026-05-12 07:00

--- public/js/menu.js
+++ public/js/menu.js
@@ -8,7 +8,7 @@ style.setAttribute('rel','stylesheet');
 document.querySelector('head').append(style);
 
 
-const menuText=['<a href="https://qucu.ru">Main</a>','<a href="https://new.qucu.ru/">Persons</a>','<a href="https://comments.qucu.ru/">Comments</a>','<a href="https://comments.qucu.ru/app-comments.apk">Download App</a>','<a href="https://new.qucu.ru/login">Login</a>','<a href="https://new.qucu.ru/register">Registration</a>','<a href="https://new.qucu.ru/about">About</a>'];
+const menuText=['<a href="https://fleamarket.qucu.ru/">Fleamarket</a>','<a href="https://qucu.ru">Main</a>','<a href="https://new.qucu.ru/">Persons</a>','<a href="https://comments.qucu.ru/">Comments</a>','<a href="https://comments.qucu.ru/app-comments.apk">Download App</a>','<a href="https://new.qucu.ru/login">Login</a>','<a href="https://new.qucu.ru/register">Registration</a>','<a href="https://new.qucu.ru/about">About</a>'];
 
 const menuAround=document.createElement("div");
 menuAround.classList.add("menu");
