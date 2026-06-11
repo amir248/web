e6255964f7e33dfb677c52fdeb88bee7ed1045a5
chikchicly <chikchicly@gmail.com>
2026-06-11 07:00:01 +0700

    Backup 2026-06-11 07:00

--- public/js/menu1.js
+++ public/js/menu1.js
@@ -8,7 +8,7 @@ style.setAttribute('rel','stylesheet');
 document.querySelector('head').append(style);
 
 
-const menuText=['<a href="https://qucu.ru/market">Fleamarket</a>','<a href="https://qucu.ru">Main</a>','<a href="https://qucu.ru/user">Persons</a>','<a href="https://qucu.ru/comments">Comments</a>','<a href="https://qucu.ru/app-comments.apk">Download App</a>','<a href="https://qucu.ru/login">Login</a>','<a href="https://qucu.ru/register">Registration</a>','<a href="https://qucu.ru/about">About</a>'];
+const menuText=['<a href="https://qucu.ru/market">Fleamarket</a>','<a href="https://qucu.ru">Main</a>','<a href="https://qucu.ru/user">Persons</a>','<a href="https://qucu.ru/comments">Comments</a>','<a href="https://qucu.ru/app-comments.apk">Download App</a>','<a href="https://qucu.ru/app-debug.apk">Download App NEW</a>','<a href="https://qucu.ru/login">Login</a>','<a href="https://qucu.ru/register">Registration</a>','<a href="https://qucu.ru/about">About</a>'];
 
 const menuAround=document.createElement("div");
 menuAround.classList.add("menu");
