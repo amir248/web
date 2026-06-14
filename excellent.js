f2e400ff5f879291beb49c75baf7aa1343b011be
chikchicly <chikchicly@gmail.com>
2026-06-14 07:00:01 +0700

    Backup 2026-06-14 07:00

--- excellent.js
+++ excellent.js
@@ -78,6 +78,8 @@ const upload = multer({ storage: storage });
 app.set('view engine', 'ejs');
 // app.set("views", path.join(__dirname, "views"));
 // app.use(express.static(path.join(__dirname, "views")));
+// Открываем папку public как статическую
+app.use(express.static(path.join(__dirname, 'public_old')));
 app.use(express.static(path.join(__dirname, "public"))); // !!!!
 app.use('/uploads', express.static(path.join(__dirname, 'public/uploads')));
 
